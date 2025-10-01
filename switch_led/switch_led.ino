5f6be3508e364368326422d32fd7f25602c1462e
Yang-shu-yi <cvsd02486@gmail.com>
2025-10-01 14:59:25 +0800

    level2_switch_led

--- switch_led/switch_led.ino
+++ switch_led/switch_led.ino
@@ -6,58 +6,71 @@ const int gLED=5;
 int state=0;
 int ledcolor=0;
 
+bool pressbutton= 0;
+String currentcolor="led";
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(buttonPin,INPUT);
   pinMode(rLED,OUTPUT);
   pinMode(bLED,OUTPUT);
   pinMode(gLED,OUTPUT);
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   state=digitalRead(buttonPin);
+  Serial.print("currentcolor:");
+  Serial.println(currentcolor);
   if(state==0){
     ledcolor = ledcolor + 1;
     delay(100);
   }
   if(ledcolor==0){
+    currentcolor="LED off";
     digitalWrite(rLED,1);
     digitalWrite(bLED,1);
     digitalWrite(gLED,1);
   }
   else if(ledcolor==1){
+    currentcolor="Red";
     digitalWrite(rLED,0);
     digitalWrite(bLED,1);
     digitalWrite(gLED,1);
   }
   else if(ledcolor==2){
+    currentcolor="Green";
     digitalWrite(rLED,1);
     digitalWrite(bLED,0);
     digitalWrite(gLED,1);
   }
   else if(ledcolor==3){
+    currentcolor="Blue";
     digitalWrite(rLED,1);
     digitalWrite(bLED,1);
     digitalWrite(gLED,0);
   }
   else if(ledcolor==4){
+    currentcolor="purple";
     digitalWrite(rLED,0);
     digitalWrite(bLED,0);
     digitalWrite(gLED,1);
   }
   else if(ledcolor==5){
+    currentcolor="Yellow";
     digitalWrite(rLED,0);
     digitalWrite(bLED,1);
     digitalWrite(gLED,0);
   }
   else if(ledcolor==6){
+    currentcolor="Cyan";
     digitalWrite(rLED,1);
     digitalWrite(bLED,0);
     digitalWrite(gLED,0);
   }
   else if(ledcolor==7){
+    currentcolor="White";
     digitalWrite(rLED,0);
     digitalWrite(bLED,0);
     digitalWrite(gLED,0);
